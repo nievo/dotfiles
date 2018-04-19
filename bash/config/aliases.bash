5854b738bba608a78b9aa407d2b9e7dafe35b9b9
Olly Nevard <olly.nevard@housesimple.com>
2018-04-19 06:50:18 +0100

    Add tmux config

--- bash/config/aliases.bash
+++ bash/config/aliases.bash
@@ -2,4 +2,5 @@ alias ls='ls --color=auto'
 alias ll='ls -alh'
 
 alias ssh='TERM=xterm-256color ssh'
+alias tmux='TERM=xterm-256color tmux -f "$XDG_CONFIG_HOME/tmux/tmux.conf"'
 alias vagrant='TERM=xterm-256color vagrant'
